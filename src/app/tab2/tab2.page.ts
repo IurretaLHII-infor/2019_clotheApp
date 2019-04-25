3a13aad3d2a81fb282535f4c13304d42f8141cea
JonCas22 <jon.castillo@maristak.net>
2019-04-25 14:50:33 +0200

    Login button dinamic change, delete button add but not functional, modal close on login

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -34,10 +34,10 @@ export class Tab2Page {
     db.collection('ClothesProductList').get().then(function (querySnapshot) {
     querySnapshot.forEach(function (doc) {
         console.log(doc.id, ' => ', doc.data());
-        self.clotheList.push(doc.data());
+        self.clotheList.push(doc);
         });
       });
-    }
+  }
 
     async newProduct() {
         let product = { name: "", description: "" };
